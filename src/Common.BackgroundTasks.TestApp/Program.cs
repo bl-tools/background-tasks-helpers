103c8f38e34e255aa3437b04ca466d867d38a374
Alexey Fedoruk <alexey_fedoruk@stylesoft.by>
2020-07-21 19:35:46 +0300

    Add initial solution

---
+++ src/Common.BackgroundTasks.TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Common.BackgroundTasks.TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(DependencyConfig.ConfigureServices)
+        ;
+    }
+}
